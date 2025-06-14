a17eb2e71980098775365c0a4a592fa37fb8c215
Yasin Karaaslan <yasink.aslan51@gmail.com>
2025-06-14 14:40:39 +0300

    Add colored_cube example

---
+++ examples/colored_cube/colored_cube.hlsl
@@ -0,0 +1,17 @@
+// Vertex Shader
+cbuffer Transform_Buffer {
+    matrix transformation;
+}
+
+float4 vertex_main(float3 Pos : POSITION) : SV_Position {
+    return mul(float4(Pos, 1), transformation);
+}
+
+// Fragment Shader
+cbuffer Colors_Buffer {
+    float4 face_colors[6];
+}
+
+float4 fragment_main(uint triangle_id: SV_PrimitiveID) : SV_Target {
+    return face_colors[triangle_id/2];
+}
