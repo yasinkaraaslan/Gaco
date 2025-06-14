448fc3e95cc653d3ad194884f7bb31a1bb2b9c4d
Yasin Karaaslan <yasink.aslan51@gmail.com>
2025-06-14 12:21:29 +0300

    Make triangle example colorful, add set_shader to opengl

--- examples/triangle/triangle.hlsl
+++ examples/triangle/triangle.hlsl
@@ -1,8 +1,15 @@
+struct Fragment_Input {
+    float3 color : COLOR;
+    float4 sv_pos: SV_Position;
+};
 
-float4 vertex_main(float3 Pos : POSITION) : SV_Position {
-    return float4(Pos, 1);
-}
+Fragment_Input vertex_main(float3 Pos : POSITION) {
+    Fragment_Input output;
+    output.color = Pos + 0.5;
+    output.sv_pos = float4(Pos, 1);
+    return output;
+};
 
-float4 fragment_main(float4 Pos : SV_Position) : SV_Target {
-    return float4(0.2, 0.5, 0.5, 1);
+float4 fragment_main(Fragment_Input input) : SV_Target {
+    return float4(input.color, 1);
 }
