194e1ad01076e0c4eadfab36f3f98cef404be063
Yasin Karaaslan <yasin@yasinkaraaslan.com>
2026-08-07 18:42:09 +0300

    OpenGL compute only context creation for Windows

--- examples/compute/compute.hlsl
+++ examples/compute/compute.hlsl
@@ -7,9 +7,10 @@ RWStructuredBuffer<float> output : register(u1);
 static const uint NumX = 69;
 static const uint NumY = 105;
 [numthreads(1,1,1)]
-void compute_main(uint3 DTid : SV_DispatchThreadID ) {
+void compute_main(uint3 DTid : SV_DispatchThreadID) {
    uint index = DTid.x + DTid.y * NumX;
 
+   // We do some stuff to see the result
    float other_num1 = input[index] * input[index];
    float other_num2 = other_num1 * (input[index] + input[index]);
    output[index] = other_num1 + other_num2;
