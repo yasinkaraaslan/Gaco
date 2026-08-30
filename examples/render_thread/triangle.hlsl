c21b68c93d21baa46ddce3775c394dccfb376eb0
Yasin Karaaslan <yasin@yasinkaraaslan.com>
2026-08-31 00:45:57 +0300

    Add render_thread example

---
+++ examples/render_thread/triangle.hlsl
@@ -0,0 +1,15 @@
+struct Fragment_Input {
+    float3 color : COLOR;
+    float4 sv_pos: SV_Position;
+};
+
+Fragment_Input vertex_main(float3 Pos : POSITION) {
+    Fragment_Input output;
+    output.color = Pos + 0.5;
+    output.sv_pos = float4(Pos, 1);
+    return output;
+};
+
+float4 fragment_main(Fragment_Input input) : SV_Target {
+    return float4(input.color, 1);
+}
